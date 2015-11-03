abbd6b31acc99cbbe893274d10cc4df28fa4014c
Paulo Faria <paulo.faria.rl@gmail.com>
2015-11-03 21:32:12 -0200

    Initial Commit

---
+++ Currents/Currents.h
@@ -0,0 +1,19 @@
+//
+//  Currents.h
+//  Currents
+//
+//  Created by Paulo Faria on 11/3/15.
+//  Copyright © 2015 Zewo. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for Currents.
+FOUNDATION_EXPORT double CurrentsVersionNumber;
+
+//! Project version string for Currents.
+FOUNDATION_EXPORT const unsigned char CurrentsVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Currents/PublicHeader.h>
+
+
